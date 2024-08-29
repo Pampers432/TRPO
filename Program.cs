fa8e80db1337eb9db1608d00dbb78f4563f3fb73
Pampers432 <max23012007@gmail.com>
2024-08-29 11:02:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+class Programm 
+{ 
+    
+    private static void Main(string[] args) 
+    {
+        Door door1 = new Door();
+        door1.AddElement();
+    }
+}
+
+class Product 
+{ 
+    public string Size { get; set; } 
+
+    public string Price { get; set; }
+
+    public void AddElement() 
+    {
+        Console.Write("Введите кол-во: ");
+        Size = Console.ReadLine();
+        Console.Write("Введите стоимость: ");
+        Price = Console.ReadLine();
+    }
+}
+
+class Door : Product 
+{
+    
+}
+
+class Window : Product 
+{
+    
+}
