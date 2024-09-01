417b06a60ebbe3030b0e1acdabbcdff56a87174f
Pampers432 <max23012007@gmail.com>
2024-09-01 13:52:45 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Классы_в_курсовой
+{
+    abstract class Product
+    {
+        protected string Size { get; set; }
+
+        protected string Price { get; set; }
+
+        //public void AddElement()
+        //{
+        //    Console.Write("Введите кол-во: ");
+        //    Size = Console.ReadLine();
+        //    Console.Write("Введите стоимость: ");
+        //    Price = Console.ReadLine();
+        //}
+
+
+    }
+
+    
+}
---
+++ SQLProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Классы_в_курсовой
+{
+    class SQLDoor : Product
+    {
+        public SQLDoor(string size, string price)
+        {
+            Size = size;
+            Price = price;
+        }
+    }
+
+    class SQLWindow : Product
+    {
+        public SQLWindow(string size, string price)
+        {
+            Size = size;
+            Price = price;
+        }
+    }
+}
