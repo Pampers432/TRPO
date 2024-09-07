281bed6e543421582e9f22506e0b109255a1d20c
Pampers432 <max23012007@gmail.com>
2024-09-07 20:35:54 +0300

    Add files via upload

---
+++ DoorsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestShop.Data.intarfaces;
+using TestShop.ViewModels;
+
+namespace TestShop.Controllers
+{
+    public class DoorsController : Controller
+    {
+        private readonly IDoor _allDoors;
+
+        private readonly ICategory _allCategories;
+
+        public DoorsController(IDoor iDoor, ICategory iCategory)
+        {
+            _allDoors = iDoor;
+            _allCategories = iCategory;
+        }
+
+        public ViewResult List()
+        {
+            DoorsListViewModel obj = new();
+            obj.allDoors = _allDoors.AllDoors;
+            obj.doorCategory = "Двери";
+            return View(obj);
+        }
+    }
+}
---
+++ DoorsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TestShop.Data.Models;
+
+namespace TestShop.ViewModels
+{
+    public class DoorsListViewModel
+    {
+        public IEnumerable<Door> allDoors {  get; set; }
+
+        public string doorCategory { get; set; }
+    }
+}
